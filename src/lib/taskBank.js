f036966405de4b900de0763aae3a72970c1f4488
FalseGhost <amitojsingh8070@gmail.com>
2022-03-05 12:51:39 -0500

    .

--- src/lib/taskBank.js
+++ src/lib/taskBank.js
@@ -78,106 +78,79 @@ const taskBank = [
 
 const taskBank = [
     {
-        "title": "Share on your local neighborhood FB group or NextDoor group one local candidate you're voting for and why.",
+        "title": "Box dosen't spawn",
     },
     {
-        "title": "Encourage your social media audience to fill out the 2020 Census.",
+        "title": "They forgot the Vulnerability",
     },
     {
-        "title": "Sign up to write letters to likely voters with @votefwd",
+        "title": "Mods say to stay on-topic",
     },
     {
-        "title": "Text 5 people and make sure they have a voting plan.",
+        "title": "Blind SQL Injection",
     },
     {
-        "title": "For parents of young children: ask your daycare what the Election day plans are so teachers can vote.",
+        "title": "Nmap Scan through Proxy/Slow AF",
     },
     {
-        "title": "Sign up to phone bank! Your local county democratic party is likely phone banking for close races at the state level!",
+        "title": "ignore me hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee hee ",
     },
     {
-        "title": "If you're not in a swing state, adopt a state!",
+        "title": "Exploit on Author's github",
     },
     {
-        "title": "Commit to volunteer during the last weekends before the election.",
+        "title": "XSS with unneccesary complex scripting",
     },
     {
-        "title": "Become a poll watcher for election day.",
+        "title": "Exploit on Box",
     },
     {
-        "title": "Support these Democratic House campaigns in battleground states.",
+        "title": "Unintended root within 2 hours",
     },
     {
-        "title": "Thank a reporter on social media. They are doing hard work and have had a very hard year.",
+        "title": "Broken on Release Arena",
     },
     {
-        "title": "Donate to a local news organization that is pounding the pavement and breaking stories.",
+        "title": "Obnoxious WAF",
     },
     {
-        "title": "Call your local senior center and see if they need drivers to take people to the polls.",
+        "title": "Random dude asks for nudges 10 minutes in",
     },
     {
-        "title": "Calcuate your time on social media and use that time the next day to write letters!",
+        "title": "Have to Download lots of stuff",
     },
     {
-        "title": "Take a day off!",
+        "title": "Have to set up a complex/wierd lab environment",
     },
     {
-        "title": "Donate your tech skills for a campaign.",
+        "title": "Malware on the box",
     },
     {
-        "title": "Volunteer to translate material for a local candidate.",
+        "title": "Just a challenge disguised as a box",
     },
     {
-        "title": "Check your voter registration status.",
+        "title": "syzmex73 get's user blood",
     },
     {
-        "title": "Make your voting plan.",
+        "title": "placeholder 1",
     },
     {
-        "title": "Look at this spreadsheet of flippable seats and commit to donating to one of them.",
+        "title": "placeholder 2",
     },
     {
-        "title": "Put the Election Protection Hotline number in your phone: 1-866-OUR-VOTE",
+        "title": "placeholder 3",
     },
     {
-        "title": "Donate to the Southern Coalition for Social Justice.",
+        "title": "placeholder 4",
     },
     {
-        "title": "Read a sample ballot, and be sure you're ready to vote in every race in your area!",
+        "title": "placeholder 5",
     },
     {
-        "title": "Write postcards.",
+        "title": "placeholder 6",
     },
     {
-        "title": "Text canvass voters!",
-    },
-    {
-        "title": "Sign up for 2020 Victory National Text Team Training!",
-    },
-    {
-        "title": "Give your brain a break and stop doomscrolling.",
-    },
-    {
-        "title": "Resource your local anti-racism movement.",
-    },
-    {
-        "title": "Plan for self-care and community care in November and the years to come.",
-    },
-    {
-        "title": "Fund voter protection and registration for people of color. Some choices: @votolatino, @BlackVotersMtr, @PIVOTorg!",
-    },
-    {
-        "title": "Offer to witness ballots for people in your network who live alone",
-    },
-    {
-        "title": "Host a virtual textbanking event!",
-    },
-    {
-        "title": "Volunteer with the DSCC to protect voting rights",
-    },
-    {
-        "title": "Join People's Action battleground states Deep Canvass Phonebank!",
+        "title": "placeholder 7",
     }
 ];
 
