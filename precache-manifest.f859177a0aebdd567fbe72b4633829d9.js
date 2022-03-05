b7684ab37b7f7525e61bfe1167280f39c8576299
FalseGhost <amitojsingh8070@gmail.com>
2022-03-04 20:12:20 -0500

    Updates

---
+++ precache-manifest.f859177a0aebdd567fbe72b4633829d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedba5c8dd4b90eef1931640622b3ad8",
+    "url": "/october-bingo/index.html"
+  },
+  {
+    "revision": "21cbc6c13a6a3e7cbb1b",
+    "url": "/october-bingo/static/css/main.992fd86d.chunk.css"
+  },
+  {
+    "revision": "5a690b39691fcd9c5068",
+    "url": "/october-bingo/static/js/2.8aca7923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/october-bingo/static/js/2.8aca7923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cbc6c13a6a3e7cbb1b",
+    "url": "/october-bingo/static/js/main.ec2defad.chunk.js"
+  },
+  {
+    "revision": "18e72de56c2f0d6aab70",
+    "url": "/october-bingo/static/js/runtime-main.ef03ba89.js"
+  }
+]);
