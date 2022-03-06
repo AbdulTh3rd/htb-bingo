5480c99769639db7058e7d01a38e8f69f8c93337
FalseGhost <amitojsingh8070@gmail.com>
2022-03-05 20:27:25 -0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,10 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <footer className="Footer">
+            <h2>s</h2>
+            <h2>s</h2>
+            <h2>s</h2>
+            <h2>s</h2>
         </footer>
     )
 
